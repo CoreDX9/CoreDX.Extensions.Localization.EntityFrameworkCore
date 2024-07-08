e57f98aa18520c5f45ff6d2e73fd89fa5bc2b2f2
Ao Rui <coredx@live.cn>
2024-07-08 21:13:20 +0800

    Update comment.

--- src/CoreDX.Extensions.Localization.EntityFrameworkCore.Models/LocalizationRecord.cs
+++ src/CoreDX.Extensions.Localization.EntityFrameworkCore.Models/LocalizationRecord.cs
@@ -1,6 +1,8 @@
 ﻿namespace CoreDX.Extensions.Localization.EntityFrameworkCore.Models;
 
+#pragma warning disable CS1591 // 缺少对公共可见类型或成员的 XML 注释
 public class LocalizationRecord
+
 {
     public virtual long Id { get; set; }
     public virtual string ResourceCulture { get; set; } = null!;
@@ -8,3 +10,4 @@ public class LocalizationRecord
     public virtual string ContentKey { get; set; } = null!;
     public virtual string? LocalizedContent { get; set; }
 }
+#pragma warning restore CS1591 // 缺少对公共可见类型或成员的 XML 注释
--- src/CoreDX.Extensions.Localization.EntityFrameworkCore.Models/ModelBuilderExtensions.cs
+++ src/CoreDX.Extensions.Localization.EntityFrameworkCore.Models/ModelBuilderExtensions.cs
@@ -3,13 +3,29 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CoreDX.Extensions.Localization.EntityFrameworkCore.Models;
 
+/// <summary>
+/// Extensions for configure entity model of type <see cref="LocalizationRecord"/>.
+/// </summary>
 public static class ModelBuilderExtensions
 {
+    /// <summary>
+    /// Configure entity model using type <see cref="LocalizationRecord"/>.
+    /// </summary>
+    /// <param name="modelBuilder">The model builder.</param>
+    /// <param name="configureEntity">Custom configuration action.</param>
+    /// <returns>An object that can be used to configure the entity type.</returns>
     public static ModelBuilder UseLocalizationRecord(
     this ModelBuilder modelBuilder,
     Action<EntityTypeBuilder<LocalizationRecord>>? configureEntity = null) =>
         modelBuilder.UseLocalizationRecord(configureEntity);
 
+    /// <summary>
+    /// Configure entity model using custom type.
+    /// </summary>
+    /// <typeparam name="TLocalizationRecord">The type of entity.</typeparam>
+    /// <param name="modelBuilder">The model builder.</param>
+    /// <param name="configureEntity">Custom configuration action.</param>
+    /// <returns>An object that can be used to configure the entity type.</returns>
     public static ModelBuilder UseLocalizationRecord<TLocalizationRecord>(
         this ModelBuilder modelBuilder,
         Action<EntityTypeBuilder<TLocalizationRecord>>? configureEntity = null)
